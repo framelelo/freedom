d56f1f74c8d0f4bd7d6211e4cf299dfd13e5b77c
framelelo <framelelo89@gmail.com>
2023-10-16 22:57:32 +0400

    Styled icons

--- templates/layout.php
+++ templates/layout.php
@@ -26,11 +26,10 @@ global $isConnected;
         <div class="main-container" id="page_<?= $title?>">
             <header class='p-4 mb-4'>
                 <?php if ($isConnected){
-                    $gender = $_SESSION["users"]["gender"];
                     $username = $_SESSION["users"]["username"];
                     ?> 
                     <a href='<?= $base_url ?>' class="username">
-                        Bonjour, <span><?= $gender . ' '. $username?></span>
+                        Bonjour, <span><?= $username?></span>
                     </a>
                     <div>
                         <a href='<?= $base_url ?>?page=logout' class="logout"><i class="fas fa-sign-out-alt"></i></a>
@@ -46,7 +45,7 @@ global $isConnected;
             </main>
         </div>
         
-        <!-- BBOSTRAP -->
+        <!-- BOOSTRAP -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </body>
 </html>
