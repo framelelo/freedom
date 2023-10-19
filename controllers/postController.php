f9798d81405377d2eba43b2a40dce1fe39c994c4
framelelo <framelelo89@gmail.com>
2023-10-19 09:40:19 +0400

    Default image added for post

--- controllers/postController.php
+++ controllers/postController.php
@@ -10,28 +10,41 @@ function CreatePostAction() {
             $user_id = $_SESSION['users']['id'];
             $post_title = $_POST['post_title'];
             $post_content = $_POST['post_content'];
-        
-            $image_name = null; 
-        
+            
+            $image_name = null;
+
+            $image_name = time() . '_' . $_FILES['post_img']['name'];
+            $temp_folder = $_FILES['post_img']['tmp_name'];
+            $upload_folder = ROOT_PATH . "/uploads/" . $image_name;
+            
             if (!empty($_FILES['post_img']['name'])) {
-                $image_name = time() . '_' . $_FILES['post_img']['name'];
-        
-                $temp_folder = $_FILES['post_img']['tmp_name'];
-                $upload_folder = ROOT_PATH . "/uploads/" . $image_name;
+                
                 
                 $result = move_uploaded_file($temp_folder, $upload_folder);
+                
                 if (!$result) {
                     echo "Merci de vérifier.";
                 }
+            } else {
+                $default_image_path = $upload_folder; 
+                $image_name = 'dafault_post_img.png'; 
+                
+                if (copy($default_image_path, ROOT_PATH . "/uploads/" . $image_name)) {
+                    echo "Default image uploaded.";
+                } else {
+                    echo "Error uploading default image.";
+                }
             }
+            
             $post = createPost($user_id, $image_name, $post_title, $post_content);
-        
+            
             if ($post) {
                 header("location: $base_url");
             } else {
                 echo '<p class="message px-2">Merci de vérifier !</p>';
             }
         }
+        
                    
         } else header("location: $base_url?page=login");
     }
